9fea98eee5d5307e9ff5226729cd0249007094f4
Dariusz Dziuk <me@dariuszdziuk.com>
2013-10-31 21:33:10 +0100

    Initial commit.

---
+++ scripts/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Setup git
+git config --global user.name "Dariusz Dziuk"
+git config --global user.email me@dariuszdziuk.com
+git config --global core.editor vim
+git config --global color.ui true 
